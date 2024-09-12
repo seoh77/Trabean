2ee31063d5e84108f7178865cc4afd987a7d027c
kiminchae <alsco9707@gmail.com>
2024-09-12 10:30:08 +0900

    fix : enum 오류 수정

--- payment/src/main/java/com/trabean/payment/enums/PaymentStatus.java
+++ payment/src/main/java/com/trabean/payment/enums/PaymentStatus.java
@@ -1,5 +1,18 @@
 package com.trabean.payment.enums;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public enum PaymentStatus {
-    SUCCESS, CANCEL, EXPIRED, PASSWORD_ERROR, BALANCE_ERROR, PENDING
+    SUCCESS, CANCEL, EXPIRED, PASSWORD_ERROR, BALANCE_ERROR, PENDING;
+
+    @JsonCreator
+    public static PaymentStatus from(String value) {
+        return PaymentStatus.valueOf(value.toUpperCase());
+    }
+
+    @JsonValue
+    public String toValue() {
+        return this.name();
+    }
 }
--- payment/src/main/java/com/trabean/payment/enums/PayoutStatus.java
+++ payment/src/main/java/com/trabean/payment/enums/PayoutStatus.java
@@ -1,5 +1,18 @@
 package com.trabean.payment.enums;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public enum PayoutStatus {
-    SUCCESS, FAILED, PENDING
+    SUCCESS, FAILED, PENDING;
+
+    @JsonCreator
+    public static PayoutStatus from(String value) {
+        return PayoutStatus.valueOf(value.toUpperCase());
+    }
+
+    @JsonValue
+    public String toValue() {
+        return this.name();
+    }
 }
