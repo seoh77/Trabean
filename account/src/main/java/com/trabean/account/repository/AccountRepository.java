d003b86ddde68095f831aacfa0ffb4b170591e30
FickleBoBo <yukmw0704@gmail.com>
2024-09-19 17:53:10 +0900

    refactor : 레포지토리 어노테이션 추가

--- account/src/main/java/com/trabean/account/repository/AccountRepository.java
+++ account/src/main/java/com/trabean/account/repository/AccountRepository.java
@@ -2,6 +2,8 @@ package com.trabean.account.repository;
 
 import com.trabean.account.domain.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AccountRepository extends JpaRepository<Account, Long> {
 }
