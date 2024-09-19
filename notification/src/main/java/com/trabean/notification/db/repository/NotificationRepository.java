97092145508ab5f0c4a2d4d037718aef35f94a66
박세건 <qkrtprjs456@naver.com>
2024-09-19 13:45:38 +0900

    test : DB 연결 확인을 위한 Controller 기능 추가

--- notification/src/main/java/com/trabean/notification/db/repository/NotificationRepository.java
+++ notification/src/main/java/com/trabean/notification/db/repository/NotificationRepository.java
@@ -3,5 +3,5 @@ package com.trabean.notification.db.repository;
 import com.trabean.notification.db.entity.Notification;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface NotificationRepository extends JpaRepository<Notification, Integer> {
+public interface NotificationRepository extends JpaRepository<Notification, Long> {
 }
