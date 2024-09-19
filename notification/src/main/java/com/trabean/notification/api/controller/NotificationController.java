f91c7b209525c33794cf34dad005b1e8a4a6977e
박세건 <qkrtprjs456@naver.com>
2024-09-19 10:14:18 +0900

    test : 마이크로서비스 운영 테스트를 위한 controller 수정

--- notification/src/main/java/com/trabean/notification/api/controller/NotificationController.java
+++ notification/src/main/java/com/trabean/notification/api/controller/NotificationController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/notification")
+//@RequestMapping("/api/notification")
 
 public class NotificationController {
 
