870f4f40d962310d3ebf153503ca78f6a6cd1aca
박세건 <qkrtprjs456@naver.com>
2024-09-19 13:26:58 +0900

    test : DB 연결 확인을 위한 Controller 기능 추가

---
+++ notification/src/main/java/com/trabean/notification/api/service/NotificationServiceImpl.java
@@ -0,0 +1,20 @@
+package com.trabean.notification.api.service;
+
+
+import com.trabean.notification.db.entity.Notification;
+import com.trabean.notification.db.repository.NotificationRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class NotificationServiceImpl implements NotificationService{
+    private final NotificationRepository notificationRepository;
+    @Override
+    public List<Notification> getNotifications() {
+        return notificationRepository.findAll();
+
+    }
+}
