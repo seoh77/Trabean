49601c119f05cbb426258c7e2d8cdeb8cf3995a5
FickleBoBo <yukmw0704@gmail.com>
2024-09-20 10:02:46 +0900

    hotfix : 환경변수 및 설정파일 변경

---
+++ account/src/main/java/com/trabean/util/Header.java
@@ -0,0 +1,38 @@
+package com.trabean.util;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@Data
+@Builder
+public class Header {
+    String apiName;
+
+    @Builder.Default
+    String transmissionDate = getCurrentDate();
+
+    @Builder.Default
+    String transmissionTime = getCurrentTime();
+
+    final String institutionCode = "00100";
+    final String fintechAppNo = "001";
+
+    @Builder.Default
+    String apiServiceCode = apiName;
+
+    String institutionTransactionUniqueNo;
+    String apiKey;
+    String userKey;
+
+    private static String getCurrentDate(){
+        return LocalDate.now().format(DateTimeFormatter.ofPattern("yyyyMMdd"));
+    }
+
+    private static String getCurrentTime(){
+        return LocalDateTime.now().format(DateTimeFormatter.ofPattern("HHmmss"));
+    }
+}
