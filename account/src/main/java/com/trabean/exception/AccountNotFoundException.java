59539efc9218ca8d425ed732f909f9a054a6700d
FickleBoBo <yukmw0704@gmail.com>
2024-09-23 00:26:13 +0900

    refactor: 전역 예외 클래스 리팩토링

--- account/src/main/java/com/trabean/exception/AccountNotFoundException.java
+++ account/src/main/java/com/trabean/exception/AccountNotFoundException.java
@@ -1,7 +1,9 @@
 package com.trabean.exception;
 
 public class AccountNotFoundException extends RuntimeException {
+
     public AccountNotFoundException(String message) {
         super(message);
     }
+
 }
--- account/src/main/java/com/trabean/exception/AccountRecentlyCreatedException.java
+++
@@ -1,7 +0,0 @@
-package com.trabean.exception;
-
-public class AccountRecentlyCreatedException extends RuntimeException {
-    public AccountRecentlyCreatedException(String message) {
-        super(message);
-    }
-}
