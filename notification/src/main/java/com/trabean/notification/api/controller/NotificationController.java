022ed53886733eb960b4f8ada7cb99308fba4a62
박세건 <qkrtprjs456@naver.com>
2024-09-23 09:24:21 +0900

    refactor : API 컨벤션에 맞게 코드 수정

--- notification/src/main/java/com/trabean/notification/api/controller/NotificationController.java
+++ notification/src/main/java/com/trabean/notification/api/controller/NotificationController.java
@@ -13,7 +13,7 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-//@RequestMapping("/api/notification")
+@RequestMapping("/api/notification")
 
 public class NotificationController {
 
