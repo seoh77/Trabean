3047137b832e69d8adf0f048487a496ad1900dda
박세건 <qkrtprjs456@naver.com>
2024-09-25 16:31:47 +0900

    docs : Eureka 차이를 위한 테스트 name 수정

--- notification/src/main/java/com/trabean/notification/api/controller/FeignController.java
+++ notification/src/main/java/com/trabean/notification/api/controller/FeignController.java
@@ -4,7 +4,7 @@ import com.trabean.notification.api.dto.feignClient.TestDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@FeignClient(name = "notification", url = "http://j11a604.p.ssafy.io:8083", path = "/api")
+@FeignClient(name = "notification1", url = "http://j11a604.p.ssafy.io:8083", path = "/api")
 public interface FeignController {
     @GetMapping("/notification/test")
     TestDto tttt();
