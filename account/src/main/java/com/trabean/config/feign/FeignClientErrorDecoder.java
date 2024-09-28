dc71873de42c1c32ff4bd54df3da795c0584a8dc
FickleBoBo <yukmw0704@gmail.com>
2024-09-29 05:43:31 +0900

    rename : 조건문 구조 변경

--- account/src/main/java/com/trabean/config/feign/FeignClientErrorDecoder.java
+++ account/src/main/java/com/trabean/config/feign/FeignClientErrorDecoder.java
@@ -16,9 +16,7 @@ public class FeignClientErrorDecoder implements ErrorDecoder {
         ObjectMapper objectMapper = new ObjectMapper();
 
         try {
-            if(methodKey.startsWith("DomesticClient") ||
-                    methodKey.startsWith("ForeignClient") ||
-                    methodKey.startsWith("VerificationClient")) {
+            if(methodKey.startsWith("DomesticClient") || methodKey.startsWith("ForeignClient") || methodKey.startsWith("VerificationClient")) {
                 SsafyErrorResponseDTO errorResponse = objectMapper.readValue(response.body().asInputStream(), SsafyErrorResponseDTO.class);
                 return new SsafyErrorException(errorResponse);
             }
