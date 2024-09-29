4006ac827ca06cbc57fc34348ccbce1e178388f1
FickleBoBo <yukmw0704@gmail.com>
2024-09-29 22:30:18 +0900

    feat : 입금 테스트 서비스 메모 기능 추가

--- account/src/main/java/com/trabean/test/service/TestService.java
+++ account/src/main/java/com/trabean/test/service/TestService.java
@@ -4,6 +4,8 @@ import com.trabean.common.SsafySuccessResponseDTO;
 import com.trabean.external.ssafy.domestic.client.DomesticClient;
 import com.trabean.external.ssafy.domestic.dto.request.UpdateDemandDepositAccountDepositRequestDTO;
 import com.trabean.external.ssafy.domestic.dto.response.UpdateDemandDepositAccountDepositResponseDTO;
+import com.trabean.external.ssafy.memo.client.MemoClient;
+import com.trabean.external.ssafy.memo.dto.request.TransactionMemoRequestDTO;
 import com.trabean.interceptor.UserHeaderInterceptor;
 import com.trabean.test.dto.request.DepositRequestDTO;
 import com.trabean.util.RequestHeader;
@@ -15,6 +17,7 @@ import org.springframework.stereotype.Service;
 public class TestService {
 
     private final DomesticClient domesticClient;
+    private final MemoClient memoClient;
 
     // 계좌 입금 서비스 로직
     public SsafySuccessResponseDTO deposit(DepositRequestDTO requestDTO) {
@@ -30,6 +33,18 @@ public class TestService {
                 .transactionSummary(requestDTO.getTransactionSummary())
                 .build();
         UpdateDemandDepositAccountDepositResponseDTO updateDemandDepositAccountDepositResponseDTO = domesticClient.updateDemandDepositAccountDeposit(updateDemandDepositAccountDepositRequestDTO);
+
+        // SSAFY 금융 API 거래내역 메모 요청
+        TransactionMemoRequestDTO transactionMemoRequestDTO = TransactionMemoRequestDTO.builder()
+                .header(RequestHeader.builder()
+                        .apiName("transactionMemo")
+                        .userKey(UserHeaderInterceptor.userKey.get())
+                        .build())
+                .accountNo(requestDTO.getAccountNo())
+                .transactionUniqueNo(updateDemandDepositAccountDepositResponseDTO.getRec().getTransactionUniqueNo())
+                .transactionMemo(String.valueOf(UserHeaderInterceptor.userId.get()))
+                .build();
+        memoClient.transactionMeno(transactionMemoRequestDTO);
 
         return SsafySuccessResponseDTO.builder()
                 .responseCode(updateDemandDepositAccountDepositResponseDTO.getHeader().getResponseCode())
