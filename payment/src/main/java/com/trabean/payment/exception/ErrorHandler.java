0469499e3d165f4aa266cb3cb5be7fabfe56c52d
kiminchae <alsco9707@gmail.com>
2024-09-29 14:59:25 +0900

    feat : 커스텀 에러 핸들러 작성

---
+++ payment/src/main/java/com/trabean/payment/exception/ErrorHandler.java
@@ -0,0 +1,17 @@
+package com.trabean.payment.exception;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ErrorHandler {
+    @ExceptionHandler(PaymentsException.class)
+    public ResponseEntity<ErrorResponse> handlePaymentsException(PaymentsException ex) {
+
+        // 사용자에게 보여줄 커스텀 응답 객체
+        ErrorResponse errorResponse = new ErrorResponse(ex.getMessage(), ex.getStatus().value(), ex.getKrwPrice());
+
+        return ResponseEntity.status(ex.getStatus()).body(errorResponse);
+    }
+}
