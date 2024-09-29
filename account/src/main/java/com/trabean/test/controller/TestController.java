31cd849ec51ac35a067020a7ddce7cd0b5ffd125
FickleBoBo <yukmw0704@gmail.com>
2024-09-29 21:13:13 +0900

    feat : 테스트용 입금 API 작성

---
+++ account/src/main/java/com/trabean/test/controller/TestController.java
@@ -0,0 +1,25 @@
+package com.trabean.test.controller;
+
+import com.trabean.common.SsafySuccessResponseDTO;
+import com.trabean.test.dto.request.DepositRequestDTO;
+import com.trabean.test.service.TestService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin
+@RequiredArgsConstructor
+@RequestMapping("/api/accounts/test")
+public class TestController {
+
+    private final TestService testService;
+
+    // 계좌 입금 API
+    @PostMapping("/deposit")
+    ResponseEntity<SsafySuccessResponseDTO> deposit(@RequestBody DepositRequestDTO requestDTO) {
+        SsafySuccessResponseDTO responseDTO = testService.deposit(requestDTO);
+        return new ResponseEntity<>(responseDTO, HttpStatus.OK);
+    }
+}
---
+++ account/src/main/java/com/trabean/test/service/TestService.java
@@ -0,0 +1,40 @@
+package com.trabean.test.service;
+
+import com.trabean.common.SsafySuccessResponseDTO;
+import com.trabean.external.ssafy.domestic.client.DomesticClient;
+import com.trabean.external.ssafy.domestic.dto.requestDTO.UpdateDemandDepositAccountDepositRequestDTO;
+import com.trabean.external.ssafy.domestic.dto.responseDTO.UpdateDemandDepositAccountDepositResponseDTO;
+import com.trabean.interceptor.UserHeaderInterceptor;
+import com.trabean.test.dto.request.DepositRequestDTO;
+import com.trabean.util.RequestHeader;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class TestService {
+
+    private final DomesticClient domesticClient;
+
+    // 계좌 입금 서비스 로직
+    public SsafySuccessResponseDTO deposit(DepositRequestDTO requestDTO) {
+
+        // SSAFY 금융 API 계좌 입금 요청
+        UpdateDemandDepositAccountDepositRequestDTO updateDemandDepositAccountDepositRequestDTO = UpdateDemandDepositAccountDepositRequestDTO.builder()
+                .header(RequestHeader.builder()
+                        .apiName("updateDemandDepositAccountDeposit")
+                        .userKey(UserHeaderInterceptor.userKey.get())
+                        .build())
+                .accountNo(requestDTO.getAccountNo())
+                .transactionBalance(requestDTO.getTransactionBalance())
+                .transactionSummary(requestDTO.getTransactionSummary())
+                .build();
+        UpdateDemandDepositAccountDepositResponseDTO updateDemandDepositAccountDepositResponseDTO = domesticClient.updateDemandDepositAccountDeposit(updateDemandDepositAccountDepositRequestDTO);
+
+        return SsafySuccessResponseDTO.builder()
+                .responseCode(updateDemandDepositAccountDepositResponseDTO.getHeader().getResponseCode())
+                .responseMessage(updateDemandDepositAccountDepositResponseDTO.getHeader().getResponseMessage())
+                .build();
+    }
+
+}
