52eb9f30233bb00e047e9ec6d6abe7f35b7ac602
FickleBoBo <yukmw0704@gmail.com>
2024-09-29 22:33:19 +0900

    comment : 상수 클래스 주석 추가

--- account/src/main/java/com/trabean/constant/Constant.java
+++ account/src/main/java/com/trabean/constant/Constant.java
@@ -1,9 +1,25 @@
 package com.trabean.constant;
 
 public class Constant {
+    
+    /**
+     * SSAFY 금융 API 공통 Header 상수
+     */
     public static final String APPLICATION_NAME = "Trabean";
+
+    /**
+     * SSAFY 금융 API 공통 Header 상수
+     */
     public static final String INSTITUTION_CODE = "00100";
+
+    /**
+     * SSAFY 금융 API 공통 Header 상수
+     */
     public static final String FINTECH_APP_NO = "001";
+
+    /**
+     * SSAFY 금융 API 공통 Header API KEY
+     */
     public static final String API_KEY = System.getenv("API_KEY");
 
     /**
