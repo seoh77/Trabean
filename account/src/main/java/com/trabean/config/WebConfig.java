8b5fcf139296ecfee3680801b4c2869bd8f878ad
FickleBoBo <yukmw0704@gmail.com>
2024-10-02 14:35:37 +0900

    fix : CORS 설정 추가

--- account/src/main/java/com/trabean/config/WebConfig.java
+++ account/src/main/java/com/trabean/config/WebConfig.java
@@ -3,6 +3,7 @@ package com.trabean.config;
 import com.trabean.interceptor.UserHeaderInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -11,6 +12,14 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Autowired
     private UserHeaderInterceptor userHeaderInterceptor;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*");
+    }
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
