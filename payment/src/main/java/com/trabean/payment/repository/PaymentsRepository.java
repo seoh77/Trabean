9e2ccc075fef67436796d57f1971e05d368bb6a2
kiminchae <alsco9707@gmail.com>
2024-10-02 15:17:58 +0900

    fix : 결제 내역 날짜순 정렬

--- payment/src/main/java/com/trabean/payment/repository/PaymentsRepository.java
+++ payment/src/main/java/com/trabean/payment/repository/PaymentsRepository.java
@@ -13,7 +13,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PaymentsRepository extends JpaRepository<Payments, Long> {
-    @Query("SELECT p FROM Payments p WHERE p.accountId = :accountId AND p.paymentStatus = 'SUCCESS' AND p.paymentDate BETWEEN :startdate AND :enddate")
+    @Query("SELECT p FROM Payments p WHERE p.accountId = :accountId AND p.paymentStatus = 'SUCCESS' AND p.paymentDate BETWEEN :startdate AND :enddate ORDER BY p.paymentDate DESC")
     Page<Payments> findAllByAccountIdDateRange(
             @Param("accountId") Long accountId,
             @Param("startdate") LocalDateTime startdate,
@@ -25,7 +25,8 @@ public interface PaymentsRepository extends JpaRepository<Payments, Long> {
             + "FROM Payments p "
             + "WHERE p.accountId = :accountId AND p.paymentStatus = 'SUCCESS' "
             + "AND p.paymentDate BETWEEN :startdate AND :enddate "
-            + "GROUP BY p.merchant.category")
+            + "GROUP BY p.merchant.category"
+    )
     List<CategorySummary> findCategorySummaryByAccountIdAndDateRange(
             @Param("accountId") Long accountId,
             @Param("startdate") LocalDateTime startdate,
@@ -34,7 +35,7 @@ public interface PaymentsRepository extends JpaRepository<Payments, Long> {
     @Query("SELECT p FROM Payments p JOIN p.merchant m "
             + "WHERE p.accountId = :accountId AND m.category = :categoryName "
             + "AND p.paymentStatus = 'SUCCESS' "
-            + "AND p.paymentDate BETWEEN :startdate AND :enddate")
+            + "AND p.paymentDate BETWEEN :startdate AND :enddate ORDER BY p.paymentDate DESC")
     Page<Payments> findAllByCategoryAndDateRange(
             @Param("accountId") Long accountId,
             @Param("startdate") LocalDateTime startdate,
