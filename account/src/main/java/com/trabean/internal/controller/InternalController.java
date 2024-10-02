e81d5bc1f86a9484b54b8738610e32adbe07254c
FickleBoBo <yukmw0704@gmail.com>
2024-10-02 16:08:38 +0900

    fix : CORS 설정 변경

--- account/src/main/java/com/trabean/internal/controller/InternalController.java
+++ account/src/main/java/com/trabean/internal/controller/InternalController.java
@@ -12,7 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@CrossOrigin
+@CrossOrigin(origins = "*", allowCredentials = "true")
 @RequiredArgsConstructor
 @RequestMapping("/api/accounts/internal")
 public class InternalController {
