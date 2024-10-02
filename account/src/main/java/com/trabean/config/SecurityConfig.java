9478fde099543856f0785d23b5e4b8d92b796871
FickleBoBo <yukmw0704@gmail.com>
2024-10-02 16:12:44 +0900

    fix : CORS 설정 변경

--- account/src/main/java/com/trabean/config/SecurityConfig.java
+++ account/src/main/java/com/trabean/config/SecurityConfig.java
@@ -18,7 +18,7 @@ public class SecurityConfig {
     public CorsFilter corsFilter() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowCredentials(true);  // 자격 증명 허용
+//        config.setAllowCredentials(true);  // 자격 증명 허용
         config.addAllowedOriginPattern("*");  // 모든 도메인 허용
         config.addAllowedHeader("*");  // 모든 헤더 허용
         config.addAllowedMethod("*");  // 모든 메서드 허용 (GET, POST, OPTIONS 등)
