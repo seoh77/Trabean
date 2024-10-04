c9cf3d80696aa9724600cdcde4443d84cadd6c6a
AI-chemist97 <bdjg1029@gmail.com>
2024-10-04 13:33:15 +0900

    feat : app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,8 @@
 import { Routes, Route } from "react-router-dom";
 
+import PaymentHistory from "./pages/PaymentPage/PaymentPage.PaymentHistory";
+import PaymentPage from "./pages/PaymentPage/PaymentPage";
+import PaymentPassword from "./pages/PaymentPage/PaymentPage.Password";
 import TravelAccountPage from "./pages/TravelAccount/TravelAccountPage";
 import LoginPage from "./pages/LoginPage/LoginPage";
 import MainPage from "./pages/MainPage/MainPage";
@@ -7,6 +10,11 @@ import JoinPage from "./pages/JoinPage/JoinPage";
 import MailInfo from "./pages/JoinPage/JoinPage.MailInfo";
 import TransferList from "./pages/TransferPage/TransferPage.TransferList";
 import TransferLists from "./pages/TransferPage/TransferPage.TransferLists";
+import BankSelectionPage from "./pages/AccountCreationPage/BankSelectionPage";
+import CreationMainPage from "./pages/AccountCreationPage/CreationMainPage";
+import AccountAuthPage from "./pages/AccountCreationPage/AccountAuthPage";
+import IdentityAuthPage from "./pages/AccountCreationPage/IdentityAuthPage";
+import TravleSetupPage from "./pages/AccountCreationPage/TravelAccountSetup";
 import ExchangeRates from "./pages/Exchange/ExchangePage";
 
 function App() {
@@ -19,6 +27,12 @@ function App() {
           path="/accounts/travel/domestic"
           element={<TravelAccountPage />}
         />
+        <Route path="/payment/qr" element={<PaymentPage />} />
+        <Route
+          path="/payment/qr/:payId/:merchantName/:currency/:amount"
+          element={<PaymentPassword />}
+        />
+        <Route path="/payment/list" element={<PaymentHistory />} />
         <Route path="/join" element={<JoinPage />} />
         <Route path="/mailInfo" element={<MailInfo />} />
         <Route path="/transfer/list" element={<TransferLists />} />
