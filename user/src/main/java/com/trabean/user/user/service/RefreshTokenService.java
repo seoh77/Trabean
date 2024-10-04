dd9fef3390a74811085738f05309cfbb9b00950e
AI-chemist97 <bdjg1029@gmail.com>
2024-10-04 12:54:13 +0900

    feat : 디버그

--- user/src/main/java/com/trabean/user/user/service/RefreshTokenService.java
+++ user/src/main/java/com/trabean/user/user/service/RefreshTokenService.java
@@ -1,5 +1,7 @@
 package com.trabean.user.user.service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Service;
 
 import com.trabean.user.user.entity.RefreshToken;
@@ -7,6 +9,9 @@ import com.trabean.user.user.repository.RefreshTokenRepository;
 
 import lombok.RequiredArgsConstructor;
 
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 
 @RequiredArgsConstructor
@@ -16,11 +21,20 @@ public class RefreshTokenService {
 	private final RefreshTokenRepository refreshTokenRepository;
 
 	// refresh token 저장
-	public void saveRefreshToken(Long user_id, String email, String refreshToken) {
+	public void saveRefreshToken(Long user_id, String email, String refreshToken) throws IOException {
 		Optional<RefreshToken> existingToken = refreshTokenRepository.findByEmail(email);
+		Map<String, String> responseBody = new HashMap<>();
+		responseBody.put("userKey", String.valueOf(user_id));
+		responseBody.put("refreshToken", refreshToken);
+		responseBody.put("accessToken", email);
+		HttpServletResponse response = null;
+		// 응답 상태 설정 (OK 또는 로그인 성공)
+//        response.setStatus(HttpServletResponse.SC_OK);
+		response.getWriter().write(new ObjectMapper().writeValueAsString(responseBody));
 		if (existingToken.isPresent()) {
 			//기존에 토큰 있음 업데이트!
 			existingToken.get().updateValue(refreshToken);
+
 			refreshTokenRepository.save(existingToken.get());
 		} else {
 			// 새토큰 저장
