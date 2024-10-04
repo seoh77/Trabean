b4c394a4d3ff08ae432a0a4db299d949ecea1984
AI-chemist97 <bdjg1029@gmail.com>
2024-10-04 12:58:40 +0900

    feat : 디버그

--- user/src/main/java/com/trabean/user/user/controller/TokenApiController.java
+++ user/src/main/java/com/trabean/user/user/controller/TokenApiController.java
@@ -1,5 +1,7 @@
 package com.trabean.user.user.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,10 +18,13 @@ import lombok.RequiredArgsConstructor;
 @RestController
 public class TokenApiController {
 	private final TokenService tokenService;
+	private static final Logger logger = LoggerFactory.getLogger(UserApiController.class); // 로그를 위한 Logger 추가
+
 
 	@PostMapping("/api/token")
 	public ResponseEntity<CreateAccessTokenResponse> createNewAccessToken(@RequestBody CreateAccessTokenRequest request) {
 		String newAccessToken = tokenService.createNewAccessToken(request.refreshToken());
+		logger.info("여기왔지롱");
 
 		return ResponseEntity.status(HttpStatus.CREATED).body(new CreateAccessTokenResponse(newAccessToken));
 	}
--- user/src/main/java/com/trabean/user/user/service/RefreshTokenService.java
+++ user/src/main/java/com/trabean/user/user/service/RefreshTokenService.java
@@ -1,7 +1,5 @@
 package com.trabean.user.user.service;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Service;
 
 import com.trabean.user.user.entity.RefreshToken;
@@ -9,9 +7,6 @@ import com.trabean.user.user.repository.RefreshTokenRepository;
 
 import lombok.RequiredArgsConstructor;
 
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Optional;
 
 @RequiredArgsConstructor
@@ -21,20 +16,11 @@ public class RefreshTokenService {
 	private final RefreshTokenRepository refreshTokenRepository;
 
 	// refresh token 저장
-	public void saveRefreshToken(Long user_id, String email, String refreshToken) throws IOException {
+	public void saveRefreshToken(Long user_id, String email, String refreshToken) {
 		Optional<RefreshToken> existingToken = refreshTokenRepository.findByEmail(email);
-		Map<String, String> responseBody = new HashMap<>();
-		responseBody.put("userKey", String.valueOf(user_id));
-		responseBody.put("refreshToken", refreshToken);
-		responseBody.put("accessToken", email);
-		HttpServletResponse response = null;
-		// 응답 상태 설정 (OK 또는 로그인 성공)
-//        response.setStatus(HttpServletResponse.SC_OK);
-		response.getWriter().write(new ObjectMapper().writeValueAsString(responseBody));
 		if (existingToken.isPresent()) {
 			//기존에 토큰 있음 업데이트!
 			existingToken.get().updateValue(refreshToken);
-
 			refreshTokenRepository.save(existingToken.get());
 		} else {
 			// 새토큰 저장
