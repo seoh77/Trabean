5dfdf113e2946fd247a0bd5cae23e48bc92c7628
AI-chemist97 <bdjg1029@gmail.com>
2024-10-04 13:28:30 +0900

    feat : 환율

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,5 @@
 import { Routes, Route } from "react-router-dom";
 
-import PaymentHistory from "./pages/PaymentPage/PaymentPage.PaymentHistory";
-import PaymentPage from "./pages/PaymentPage/PaymentPage";
-import PaymentPassword from "./pages/PaymentPage/PaymentPage.Password";
 import TravelAccountPage from "./pages/TravelAccount/TravelAccountPage";
 import LoginPage from "./pages/LoginPage/LoginPage";
 import MainPage from "./pages/MainPage/MainPage";
@@ -10,11 +7,6 @@ import JoinPage from "./pages/JoinPage/JoinPage";
 import MailInfo from "./pages/JoinPage/JoinPage.MailInfo";
 import TransferList from "./pages/TransferPage/TransferPage.TransferList";
 import TransferLists from "./pages/TransferPage/TransferPage.TransferLists";
-import BankSelectionPage from "./pages/AccountCreationPage/BankSelectionPage";
-import CreationMainPage from "./pages/AccountCreationPage/CreationMainPage";
-import AccountAuthPage from "./pages/AccountCreationPage/AccountAuthPage";
-import IdentityAuthPage from "./pages/AccountCreationPage/IdentityAuthPage";
-import TravleSetupPage from "./pages/AccountCreationPage/TravelAccountSetup";
 import ExchangeRates from "./pages/Exchange/ExchangePage";
 
 function App() {
@@ -27,16 +19,11 @@ function App() {
           path="/accounts/travel/domestic"
           element={<TravelAccountPage />}
         />
-        <Route path="/payment/qr" element={<PaymentPage />} />
-        <Route
-          path="/payment/qr/:payId/:merchantName/:currency/:amount"
-          element={<PaymentPassword />}
-        />
-        <Route path="/payment/list" element={<PaymentHistory />} />
         <Route path="/join" element={<JoinPage />} />
         <Route path="/mailInfo" element={<MailInfo />} />
         <Route path="/transfer/list" element={<TransferLists />} />
         <Route path="/transfer/list/:account" element={<TransferList />} />
+        <Route path="/exchange" element={<ExchangeRates />} />
       </Routes>
     </div>
   );
