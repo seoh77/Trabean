1cd043b0a85a244760a795987b328ed81e2269bb
kiminchae <alsco9707@gmail.com>
2024-10-04 10:54:22 +0900

    feat : 상단바 클래스 동적으로 수정

--- frontend/src/components/TopBar.tsx
+++ frontend/src/components/TopBar.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useLocation, useNavigate } from "react-router-dom";
+import classNames from "classnames";
 
 const NavBar: React.FC = () => {
   const location = useLocation();
@@ -9,15 +10,28 @@ const NavBar: React.FC = () => {
   const currentPageName = location.pathname.split("/").pop() || "";
 
   // 경로와 페이지 명 매핑
-  // 네비게이션 바 넣을 때 본인이 알아서 매핑 해주세요
+  // 1. 네비게이션 바 넣을 때 본인이 알아서 매핑 해주세요
+  // 2. 배경색 흰색 아닐경우 밑에 리스트에도 추가해주세요
   const pageNames: { [key: string]: string } = {
-    "": "Home",
+    "": "Loading",
     qr: "QR 결제",
     list: "가계부",
   };
 
+  // 배경색이 흰색이 아닌 페이지 리스트 (여기에 추가)
+  const notWhiteBackGround: string[] = ["list"];
+
+  // 배경색 클래스 설정
+  const topBarClass = classNames(
+    "w-[360px] h-[52px] flex items-center fixed top-0 shadow-md z-100",
+    {
+      "bg-[#F4F4F5]": notWhiteBackGround.includes(currentPageName),
+      "bg-white": !notWhiteBackGround.includes(currentPageName),
+    },
+  );
+
   return (
-    <div className="w-[360px] h-[52px] flex items-center fixed top-0 bg-transparent">
+    <div className={topBarClass}>
       <p className="text-lg px-[10px] relative text-center w-full font-semibold">
         {/* 뒤로가기 버튼 */}
         <span
