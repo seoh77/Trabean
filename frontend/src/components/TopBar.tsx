6cab441dc6e53c4bfd8459f27e35af02be4e7f7b
kiminchae <alsco9707@gmail.com>
2024-10-04 11:50:12 +0900

    feat : 상단바 로고추가

--- frontend/src/components/TopBar.tsx
+++ frontend/src/components/TopBar.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import classNames from "classnames";
+import logo from "../assets/logo.png";
 
 const NavBar: React.FC = () => {
   const location = useLocation();
@@ -8,12 +9,12 @@ const NavBar: React.FC = () => {
 
   // 현재 경로로부터 페이지 이름 추출 (단순히 경로명을 표시할 수 있음)
   const currentPageName = location.pathname.split("/").pop() || "";
-
+  console.log(currentPageName);
   // 경로와 페이지 명 매핑
   // 1. 네비게이션 바 넣을 때 본인이 알아서 매핑 해주세요
   // 2. 배경색 흰색 아닐경우 밑에 리스트에도 추가해주세요
   const pageNames: { [key: string]: string } = {
-    "": "Loading",
+    "": "홈화면",
     qr: "QR 결제",
     list: "가계부",
   };
@@ -21,9 +22,12 @@ const NavBar: React.FC = () => {
   // 배경색이 흰색이 아닌 페이지 리스트 (여기에 추가)
   const notWhiteBackGround: string[] = ["list"];
 
+  // 로고를 쓰는 페이지 리스트 (여기에 추가)
+  const logoPageList: string[] = [""];
+
   // 배경색 클래스 설정
   const topBarClass = classNames(
-    "w-[360px] h-[52px] flex items-center fixed top-0 shadow-md z-100",
+    "w-[360px] h-[52px] flex items-center fixed top-0 z-100 text-gray-700",
     {
       "bg-[#F4F4F5]": notWhiteBackGround.includes(currentPageName),
       "bg-white": !notWhiteBackGround.includes(currentPageName),
@@ -31,8 +35,13 @@ const NavBar: React.FC = () => {
   );
 
   return (
-    <div className={topBarClass}>
-      <p className="text-lg px-[10px] relative text-center w-full font-semibold">
+    <div
+      className={topBarClass}
+      style={{
+        boxShadow: "0 4px 6px -1px rgba(0, 0, 0, 0.1)",
+      }}
+    >
+      <p className="text-lg px-[10px] relative text-center w-full font-semibold flex items-center">
         {/* 뒤로가기 버튼 */}
         <span
           className="absolute left-3 cursor-pointer"
@@ -42,7 +51,15 @@ const NavBar: React.FC = () => {
           ←
         </span>
         {/* 현재 페이지 정보 */}
-        {pageNames[currentPageName] || "페이지"}
+        {!logoPageList.includes(currentPageName) &&
+          (pageNames[currentPageName] || "페이지")}
+        {logoPageList.includes(currentPageName) && (
+          <img
+            src={logo}
+            alt="trabean"
+            className="w-[77px] h-[15px] absolute left-[40%]"
+          />
+        )}
       </p>
     </div>
   );
