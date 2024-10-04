dab3b046389fa25f1077d154766cfd4438cc6399
박세건 <qkrtprjs456@naver.com>
2024-10-04 19:56:08 +0900

    test : tailwind.config.js 수정 테스트2

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,28 +1,19 @@
 /** @type {import("tailwindcss").Config} */
-module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+export default {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"], // HTML 및 JS 파일 경로
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: "#6FA760",
+          light: "#C0DEB8",
+        },
+        yellow: "#FEC72A",
+      },
+    },
+    fontFamily: {
+      Pretendard: ["Pretendard"],
+    },
   },
   plugins: [],
 };
-
-// /** @type {import("tailwindcss").Config} */
-// export default {
-//   content: ["./src/**/*.{js,jsx,ts,tsx}"], // HTML 및 JS 파일 경로
-//   theme: {
-//     extend: {
-//       colors: {
-//         primary: {
-//           DEFAULT: "#6FA760",
-//           light: "#C0DEB8",
-//         },
-//         yellow: "#FEC72A",
-//       },
-//     },
-//     fontFamily: {
-//       Pretendard: ["Pretendard"],
-//     },
-//   },
-//   plugins: [],
-// };
