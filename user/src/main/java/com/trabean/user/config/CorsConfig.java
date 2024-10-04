2560f15946d051a2d5d9a39e153dc783205c2801
AI-chemist97 <bdjg1029@gmail.com>
2024-10-04 10:51:40 +0900

    feat : cors 세팅

--- user/src/main/java/com/trabean/user/config/CorsConfig.java
+++ user/src/main/java/com/trabean/user/config/CorsConfig.java
@@ -6,9 +6,6 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
-
-import com.mysql.cj.x.protobuf.MysqlxCursor.Fetch;
-
 import java.util.Arrays;
 
 
@@ -21,15 +18,14 @@ public class CorsConfig {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
         config.addAllowedOriginPattern("http://localhost:3000");
-        config.addAllowedOriginPattern("*/login");
         config.addAllowedOriginPattern("http://localhost:8080/api/user/login");
         config.addAllowedOriginPattern("https://j11a604.p.ssafy.io/api/user/login");
         config.addAllowedOriginPattern("https://j11a604.p.ssafy.io/login");
         config.addAllowedOriginPattern("http://j11a604.p.ssafy.io/login");
         config.addAllowedOriginPattern("http://j11a604.p.ssafy.io:8888/login");
         config.addAllowedOriginPattern("https://j11a604.p.ssafy.io:8888/login");
-        config.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control","Content-Type"));
-        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS","*"));
+        config.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control","Content-Type","raw","*"));
+        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "PATCH"));
         config.addExposedHeader("Authorization");
         source.registerCorsConfiguration("/**", config);
 
--- user/src/main/java/com/trabean/user/config/WebConfig.java
+++ user/src/main/java/com/trabean/user/config/WebConfig.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.Arrays;
+
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -14,10 +16,10 @@ public class WebConfig implements WebMvcConfigurer {
 
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**")
-			.allowedOriginPatterns("*/login")
-			.allowedMethods("*")
-				.allowedHeaders("*")
-			.allowCredentials(true); //쿠키 인증 요청 허용
-		}
+		registry.addMapping("/**")  // 모든 경로에 대해 CORS 허용
+				.allowedOriginPatterns(baseUrl,"http://localhost:3000","http://localhost:8080/api/user/login","https://j11a604.p.ssafy.io/api/user/login","https://j11a604.p.ssafy.io/login","http://j11a604.p.ssafy.io/login","http://j11a604.p.ssafy.io:8888/login","https://j11a604.p.ssafy.io:8888/login") // 허용할 origin 설정
+				.allowedMethods("*")  // 모든 HTTP 메소드 허용 (GET, POST 등)
+				.allowedHeaders("*")  // 모든 헤더 허용
+				.allowCredentials(true);  // 쿠키 인증 요청 허용
+	}
 }
