6fab32cd76cbd2c99ec8c5fbec8fbbbf48b827c0
seoh7dev <seoh7dev@gmail.com>
2024-10-07 09:48:31 +0900

    feat : 로그인 후 메인화면으로 이동

--- frontend/src/pages/LoginPage/LoginPage.tsx
+++ frontend/src/pages/LoginPage/LoginPage.tsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { useState } from "react";
 
+import { useNavigate } from "react-router-dom";
 import bean from "../../assets/bean.png";
 import userIcon from "../../assets/icon/userIcon.png";
 import keyIcon from "../../assets/icon/keyIcon.png";
@@ -8,6 +9,8 @@ import useAuthStore from "../../store/useAuthStore";
 import client from "../../client";
 
 function LoginPage() {
+  const navigate = useNavigate();
+
   const [email, setEmail] = useState<string | null>(null);
   const [password, setPassword] = useState<string | null>(null);
 
@@ -41,10 +44,13 @@ function LoginPage() {
       { withCredentials: true },
     );
 
-    const token = response.data.split(" ")[1];
-    localStorage.setItem("accessToken", token);
-    setAccessToken(token);
-    getMainPaymentAccount();
+    if (response.status === 200) {
+      const token = response.data.split(" ")[1];
+      localStorage.setItem("accessToken", token);
+      setAccessToken(token);
+      getMainPaymentAccount();
+      navigate("/");
+    }
   };
 
   return (
