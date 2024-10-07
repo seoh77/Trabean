f06ef432f84a59d3539e5b86ccd8592849d36ab7
seoh7dev <seoh7dev@gmail.com>
2024-10-07 09:20:49 +0900

    remove : 백엔드 서버 파일 삭제

--- account/src/main/java/com/trabean/account/dto/request/PersonalAccountDetailRequestDTO.java
+++
@@ -1,9 +0,0 @@
-package com.trabean.account.dto.request;
-
-import lombok.Getter;
-
-@Getter
-public class PersonalAccountDetailRequestDTO {
-    private String userKey;
-    private String accountNo;
-}
--- account/src/main/java/com/trabean/exception/UserAccountRelationNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.trabean.exception;
-
-public class UserAccountRelationNotFoundException extends RuntimeException {
-
-    public UserAccountRelationNotFoundException(String message) {
-        super(message);
-    }
-
-}
