199ad7db75d57870312e8bd07c89701b76bed07e
박세건 <qkrtprjs456@naver.com>
2024-10-08 13:02:31 +0900

    feat : 1원 인증관련 알림 기능 구현

--- notification/src/main/java/com/trabean/notification/db/entity/NotificationType.java
+++ notification/src/main/java/com/trabean/notification/db/entity/NotificationType.java
@@ -7,7 +7,8 @@ public enum NotificationType {
     INVITE("초대"),
     DEPOSIT("입금"),
     WITHDRAW("출금"),
-    PAYMENT("지불");
+    PAYMENT("지불"),
+    AUTH("인증");
 
     private final String description;
 
