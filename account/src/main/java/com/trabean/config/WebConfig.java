6953f089dd972b7fe2cc0c6900d5c0745a85e2c6
FickleBoBo <yukmw0704@gmail.com>
2024-10-08 11:41:31 +0900

    fix : 중복 설정 제거

--- account/src/main/java/com/trabean/config/WebConfig.java
+++ account/src/main/java/com/trabean/config/WebConfig.java
@@ -13,14 +13,14 @@ public class WebConfig implements WebMvcConfigurer {
     @Autowired
     private UserHeaderInterceptor userHeaderInterceptor;
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000, https://j11a604.p.ssafy.io")  // 프론트엔드 도메인
-                .allowedMethods("*")  // 모든 HTTP 메서드 허용
-                .allowedHeaders("*")
-                .allowCredentials(true);  // 쿠키 등 자격 증명 허용
-    }
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")
+//                .allowedOrigins("http://localhost:3000, https://j11a604.p.ssafy.io")  // 프론트엔드 도메인
+//                .allowedMethods("*")  // 모든 HTTP 메서드 허용
+//                .allowedHeaders("*")
+//                .allowCredentials(true);  // 쿠키 등 자격 증명 허용
+//    }
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
