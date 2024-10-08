95b1a6f7facf234c04318b7c04cbd43276425f6c
박세건 <qkrtprjs456@naver.com>
2024-10-08 15:53:35 +0900

    fix : 필터, 라우팅 관련 설정 원위치

--- gateway/src/main/java/com/trabean/gateway/filter/JwtFilter.java
+++ gateway/src/main/java/com/trabean/gateway/filter/JwtFilter.java
@@ -40,14 +40,14 @@ public class JwtFilter extends AbstractGatewayFilterFactory<JwtFilter.Config> {
     public GatewayFilter apply(Config config) {
         return (exchange, chain) -> {
             logger.info("@@@@@@@@@@jwt filter!"); // 필터가 실행됨을 알림
-
-            String requestURI = exchange.getRequest().getURI().getPath();
-            if (requestURI.equals("/api/user/login") || requestURI.equals("/api/user/signup")) {
-                // 특정 URI일 경우 바로 응답을 완료
-                logger.info(requestURI + " -> 해당 요청은 필터를 사용하지 않음!"); // 필터가 실행됨을 알림
-                return exchange.getResponse().setComplete();
-            }
-            logger.info(requestURI + " -> 해당 요청은 필터를 사용함!"); // 필터가 실행됨을 알림
+//
+//            String requestURI = exchange.getRequest().getURI().getPath();
+//            if (requestURI.equals("/api/user/login") || requestURI.equals("/api/user/signup")) {
+//                // 특정 URI일 경우 바로 응답을 완료
+//                logger.info(requestURI + " -> 해당 요청은 필터를 사용하지 않음!"); // 필터가 실행됨을 알림
+//                return exchange.getResponse().setComplete();
+//            }
+//            logger.info(requestURI + " -> 해당 요청은 필터를 사용함!"); // 필터가 실행됨을 알림
 
             HttpHeaders headers = exchange.getRequest().getHeaders(); // 요청의 HTTP 헤더를 가져옴
             String accessToken = "";
