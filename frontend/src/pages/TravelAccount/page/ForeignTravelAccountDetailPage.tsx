167883295283a053b8f891f13ec673bad2dd0a06
FickleBoBo <yukmw0704@gmail.com>
2024-10-10 00:48:48 +0900

    fix : PAYER 권한 확장

--- frontend/src/pages/TravelAccount/page/ForeignTravelAccountDetailPage.tsx
+++ frontend/src/pages/TravelAccount/page/ForeignTravelAccountDetailPage.tsx
@@ -229,8 +229,8 @@ const ForeignTravelAccountDetailPage: React.FC = () => {
                 `/accounts/travel/foreign/${parentAccountId}/charge?create=false&country=${foreignTravelAccountDetailData?.country}&exchangeCurrency=${foreignTravelAccountDetailData?.exchangeCurrency}&foreignAccountId=${accountId}`,
               );
             }}
-            className={`w-1/2 mx-auto block ${userRole === "ADMIN" ? "btn-md" : "btn-gray-lg"}`}
-            disabled={userRole !== "ADMIN"}
+            className={`w-1/2 mx-auto block ${userRole === "NONE_PAYER" ? "btn-gray-lg" : "btn-md"}`}
+            disabled={userRole === "NONE_PAYER"}
           >
             충전하기
           </button>
