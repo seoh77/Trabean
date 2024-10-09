c6f1099cfa51b912a14b728972898cde8ca6adc2
kiminchae <alsco9707@gmail.com>
2024-10-10 08:46:28 +0900

    fix : 유저 id 못가져올때 에러 처리

--- payment/src/main/java/com/trabean/payment/interceptor/UserHeaderInterceptor.java
+++ payment/src/main/java/com/trabean/payment/interceptor/UserHeaderInterceptor.java
@@ -1,11 +1,15 @@
 package com.trabean.payment.interceptor;
 
+import com.trabean.payment.exception.PaymentsException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 @Component
+@Slf4j
 public class UserHeaderInterceptor implements HandlerInterceptor {
     public static ThreadLocal<Long> userId = new ThreadLocal<>();
     public static ThreadLocal<String> userKey = new ThreadLocal<>();
@@ -20,13 +24,14 @@ public class UserHeaderInterceptor implements HandlerInterceptor {
             try {
                 userId.set(Long.parseLong(userIdHeader));
             } catch (RuntimeException e) {
-                throw new IllegalArgumentException(
+                throw new PaymentsException(
                         "유저 ID 값을 long으로 변환할 수 없습니다. 헤더 id값: " + request.getHeader("userID") + "헤더 key값"
-                                + request.getHeader("userKey"));
+                                + request.getHeader("userKey"), HttpStatus.BAD_GATEWAY);
             }
         } else {
-            userId.set(31L); // userId가 null일 경우 3으로 설정
+            throw new PaymentsException("유저 아이디를 불러올 수 없습니다. (인터셉터)", HttpStatus.BAD_GATEWAY);
         }
+        log.info("유저 아이디 가져오기 (인터셉터): ", userId.get());
 
         if (userKeyHeader != null) {
             userKey.set(userKeyHeader);
