0d192e132bafa232cf88cbffb21d1d071b5e9830
김인실 <rladlstlf24@naver.com>
2024-10-10 03:02:20 +0900

    chore : e 수정

--- chatbot/app/main.py
+++ chatbot/app/main.py
@@ -1,11 +1,10 @@
-# main.py : 프로젝트의 전체적인 환경을 설정하는 파일
-from fastapi import FastAPI, BackgroundTasks
-import httpx
+from fastapi import FastAPI
 from app.api.location.answerController import locationAnswerRouter
-from app.api.location.questionController import locationQuestionRouter  # 라우터를 불러옵니다.
+from app.api.location.questionController import locationQuestionRouter
 from app.api.prompt.promptController import promptRouter
 from starlette.middleware.cors import CORSMiddleware
 from app.api.chatbot import router
+import httpx
 
 app = FastAPI()
 
@@ -20,14 +19,13 @@ app.add_middleware(
 
 # Eureka 서버 URL 및 서비스 정보 설정
 EUREKA_SERVER_URL = "http://j11a604.p.ssafy.io:8761/eureka/"
-SERVICE_NAME = "chatbot"
+SERVICE_NAME = "CHATBOT"
 INSTANCE_IP = "0.0.0.0"
 INSTANCE_PORT = 8082
 
 # 애플리케이션을 Eureka에 등록하는 함수
 async def register_to_eureka():
     async with httpx.AsyncClient() as client:
-        # 서비스 등록 정보
         payload = {
             "instance": {
                 "instanceId": f"{SERVICE_NAME}_{INSTANCE_IP}_{INSTANCE_PORT}",
@@ -51,18 +49,13 @@ async def register_to_eureka():
                 }
             }
         }
-        # Eureka에 등록
         await client.post(f"{EUREKA_SERVER_URL}/apps/{SERVICE_NAME}", json=payload)
 
-# 애플리케이션 시작 시 Eureka에 등록
-@app.on_event("startup")
-async def startup_event():
+# lifespan 이벤트 핸들러 사용
+@app.lifespan()
+async def lifespan(app):
     await register_to_eureka()  # Eureka에 등록
-
-@app.on_event("shutdown")
-async def shutdown_event():
-    # 여기서 필요한 경우 deregister 로직을 추가할 수 있습니다
-    pass
+    yield  # 애플리케이션이 시작되고 종료될 때 실행되는 코드
 
 # 라우터 등록
 app.include_router(router, prefix="/api/chatbot")
