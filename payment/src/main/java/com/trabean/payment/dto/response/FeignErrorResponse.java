49b955a116037ee366ed5f882035f96d15f3e8c6
kiminchae <alsco9707@gmail.com>
2024-10-10 10:42:56 +0900

    fix : 빌드제발

---
+++ payment/src/main/java/com/trabean/payment/dto/response/FeignErrorResponse.java
@@ -0,0 +1,12 @@
+package com.trabean.payment.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class FeignErrorResponse {
+    private String message;
+}
