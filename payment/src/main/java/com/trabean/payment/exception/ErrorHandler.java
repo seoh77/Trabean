6ac9c545eaae8a42470f17e099550d662c46de7e
kiminchae <alsco9707@gmail.com>
2024-10-10 09:09:26 +0900

    fix : 에러핸들러 수정

--- payment/src/main/java/com/trabean/payment/exception/ErrorHandler.java
+++ payment/src/main/java/com/trabean/payment/exception/ErrorHandler.java
@@ -1,17 +1,45 @@
 package com.trabean.payment.exception;
 
+import feign.FeignException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+/**
+ * 전역 예외 처리 핸들러
+ */
 @RestControllerAdvice
 public class ErrorHandler {
+
+    /**
+     * PaymentsException 처리
+     */
     @ExceptionHandler(PaymentsException.class)
     public ResponseEntity<ErrorResponse> handlePaymentsException(PaymentsException ex) {
-
-        // 사용자에게 보여줄 커스텀 응답 객체
+        // 사용자에게 보여줄 커스텀 응답 객체 생성
         ErrorResponse errorResponse = new ErrorResponse(ex.getMessage(), ex.getStatus().value(), ex.getKrwPrice());
+        return new ResponseEntity<>(errorResponse, ex.getStatus());
+    }
 
-        return ResponseEntity.status(ex.getStatus()).body(errorResponse);
+    /**
+     * FeignClientException 처리 (외부 API 호출 실패 등)
+     */
+    @ExceptionHandler(FeignException.class)
+    public ResponseEntity<ErrorResponse> handleFeignClientException(FeignException ex) {
+        // 외부 서버 오류 메시지 처리
+        ErrorResponse errorResponse = new ErrorResponse(ex.getMessage(), HttpStatus.BAD_GATEWAY.value(), null);
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_GATEWAY);
+    }
+
+    /**
+     * 그 외 발생할 수 있는 일반적인 RuntimeException 처리
+     */
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<ErrorResponse> handleRuntimeException(RuntimeException ex) {
+        // 서버 내부 오류 메시지 처리
+        ErrorResponse errorResponse = new ErrorResponse("서버 내부 오류가 발생했습니다.", HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                null);
+        return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
