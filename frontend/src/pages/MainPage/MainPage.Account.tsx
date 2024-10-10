a161e64236c359d74888d65e547a4bdf2e4042bd
seoh7dev <seoh7dev@gmail.com>
2024-10-10 09:18:00 +0900

    design : 메인페이지 잔액 줄바꿈 css 수정

--- frontend/src/pages/MainPage/MainPage.Account.tsx
+++ frontend/src/pages/MainPage/MainPage.Account.tsx
@@ -24,19 +24,23 @@ function Account({ account }: AccountProps) {
       className="flex items-center px-3 my-3"
       onClick={() => navigate(`/accounts/travel/domestic/${account.accountId}`)}
     >
-      <img src={soyIcon} alt="통장아이콘" className="w-[30px]" />
-      <div className="flex flex-col w-[170px] truncate text-ellipsis ml-2">
-        <h5 className="text-sm font-bold">{account.accountName}</h5>
-        <span className="text-gray-700 text-[8px]">{account.accountNo}</span>
+      <img src={soyIcon} alt="통장아이콘" className="w-[30px] mr-2" />
+      <div className="w-full flex justify-between items-center">
+        <div className="flex flex-col">
+          <h5 className="text-sm font-bold truncate text-ellipsis">
+            {account.accountName}
+          </h5>
+          <span className="text-gray-700 text-[8px]">{account.accountNo}</span>
+        </div>
+        <div className="flex font-bold text-sm ">
+          {formatNumberWithCommas(account.accountBalance)}원
+          <img
+            src={arrowRIcon}
+            alt="통장상세보기아이콘"
+            className="w-[18px] h-[18px] right-0"
+          />
+        </div>
       </div>
-      <div className="font-bold text-sm ml-3 w-[80px]">
-        {formatNumberWithCommas(account.accountBalance)}원
-      </div>
-      <img
-        src={arrowRIcon}
-        alt="통장상세보기아이콘"
-        className="w-[18px] right-0"
-      />
     </div>
   );
 }
